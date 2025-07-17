d90f5faf4e09e5a85cc62937eee69b756288ca6d
Sahil Kumar <itssahilhere31@gmail.com>
2025-07-17 14:37:26 +0530

    Finished topBar.kt

--- app/src/main/java/com/example/superdex/ui/components/TopAppBar.kt
+++ app/src/main/java/com/example/superdex/ui/components/TopAppBar.kt
@@ -1,5 +1,8 @@
 package com.example.superdex.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -7,11 +10,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.superdex.R
+import com.example.superdex.ui.theme.DarkRed
 import com.example.superdex.ui.theme.LocalAppColors
 
 @Composable
@@ -21,16 +26,16 @@ fun TopBarUi(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 32.dp)
+            .background(DarkRed),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = stringResource(com.example.superdex.R.string.app_name),
-            style = MaterialTheme.typography.displayMedium.copy(
-                fontFamily = FontFamily.Default,
-                color = LocalAppColors.current.primary
-            ),
-            modifier = Modifier.align(Alignment.Center)
-        )
 
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "AppLogo",
+                alignment =Alignment.Center
+
+        )
 
     }
 }
